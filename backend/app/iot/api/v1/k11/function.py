0df7f5dd243d5cb50de88f464aa2b3fe7da76532
guhua <819070918@qq.com>
2026-02-03 17:21:39 +0800

    修改功能插件接口

--- backend/app/iot/api/v1/k11/function.py
+++ backend/app/iot/api/v1/k11/function.py
@@ -5,8 +5,9 @@
 @Author  : guhua@jiqid.com
 @Date    : 2026/01/13 16:05
 """
+from typing import Optional
 
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Body
 
 from backend.app.iot.service.messaging import MessagingService
 from backend.common.mqtt_broker import get_mqtt, MQTTBroker
@@ -14,7 +15,7 @@ from backend.app.live.agents.api_clients.weather_api import open_weather_map
 
 from backend.common.response.response_schema import ResponseSchemaModel, response_base, ResponseModel
 
-from backend.app.iot.schema.intent import WeatherParam, MusicParam, AlarmParam, ControlParam
+from backend.app.iot.schema.intent import WeatherParam
 
 router = APIRouter()
 
@@ -32,26 +33,35 @@ async def weather_function(
 
 @router.post('/music', summary='音乐操作')
 async def music_function(
-        obj: MusicParam,
         mqtt_client: MQTTBroker = Depends(get_mqtt),
+        did: str = Body(..., description='设备did'),
+        action: str = Body(..., description='操作'),
+        value: Optional[str] = Body(None, description='值'),
+        song: Optional[str] = Body(None, description='歌曲名称'),
+        artist: Optional[str] = Body(None, description='歌手名称'),
+        playlist: Optional[str] = Body(None, description='专辑名称'),
+        platform: Optional[str] = Body(None, description='播放平台'),
 ) -> ResponseModel:
     """音乐播放"""
-    messaging_service = MessagingService(mqtt_client=mqtt_client, did=obj.did)
+    messaging_service = MessagingService(mqtt_client=mqtt_client, did=did)
     await messaging_service.send_play_music(
-        action=obj.action, value=obj.value, song=obj.song, artist=obj.artist, playlist=obj.playlist,
-        platform=obj.platform)
+        action=action, value=value, song=song, artist=artist, playlist=playlist,
+        platform=platform)
 
     return response_base.success()
 
 
 @router.post('/control', summary='设备控制')
 async def control_function(
-        obj: ControlParam,
         mqtt_client: MQTTBroker = Depends(get_mqtt),
+        did: str = Body(..., description='设备did'),
+        target: str = Body(..., description='目标'),
+        action: str = Body(..., description='动作'),
+        value: Optional[str] = Body(None, description='值'),
 ) -> ResponseModel:
     """设备控制"""
-    messaging_service = MessagingService(mqtt_client=mqtt_client, did=obj.did)
-    await messaging_service.send_system_control(target=obj.target, action=obj.action, value=obj.value)
+    messaging_service = MessagingService(mqtt_client=mqtt_client, did=did)
+    await messaging_service.send_system_control(target=target, action=action, value=value)
 
     return response_base.success()
 
@@ -59,10 +69,13 @@ async def control_function(
 # P1
 @router.post('/alarm', summary='闹钟设置')
 async def alarm_function(
-        obj: AlarmParam,
         mqtt_client: MQTTBroker = Depends(get_mqtt),
+        did: str = Body(..., description='设备did'),
+        action: str = Body(..., description='操作'),
+        target_time: Optional[str] = Body(None, description='时间'),
+        message: Optional[str] = Body(None, description='消息'),
 ) -> ResponseModel:
-    messaging_service = MessagingService(mqtt_client=mqtt_client, did=obj.did)
-    await messaging_service.send_alarm(action=obj.action, target_time=obj.target_time, message=obj.message)
+    messaging_service = MessagingService(mqtt_client=mqtt_client, did=did)
+    await messaging_service.send_alarm(action=action, target_time=target_time, message=message)
 
     return response_base.success()
--- backend/app/iot/schema/intent.py
+++ backend/app/iot/schema/intent.py
@@ -19,26 +19,3 @@ class ParamBase(SchemaBase):
 class WeatherParam(ParamBase):
     city: Optional[str] = Field(None, description='城市')
     ip: Optional[str] = Field(None, description='IP地址')
-
-
-class MusicParam(ParamBase):
-    # play | pause | stop | next | prev | volume_up | volume_down | volume_set
-    action: Optional[str] = Field(None, description='操作')
-
-    value: Optional[str] = Field(None, description='值')
-    song: Optional[str] = Field(None, description='歌曲名称')
-    artist: Optional[str] = Field(None, description='歌手名称')
-    playlist: Optional[str] = Field(None, description='专辑名称')
-    platform: Optional[str] = Field(None, description='播放平台')
-
-
-class AlarmParam(ParamBase):
-    action: Optional[str] = Field(None, description='操作')
-    target_time: Optional[str] = Field(None, description='时间')
-    message: Optional[str] = Field(None, description='消息')
-
-
-class ControlParam(ParamBase):
-    target: Optional[str] = Field(None, description='目标')
-    action: Optional[str] = Field(None, description='动作')
-    value: Optional[str] = Field(None, description='值')
