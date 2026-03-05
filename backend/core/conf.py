17442b81ff626281c16568ebcfc67c6ad99547fe
guhua <819070918@qq.com>
2026-03-05 20:17:59 +0800

    fix

--- backend/core/conf.py
+++ backend/core/conf.py
@@ -185,7 +185,7 @@ class Settings(BaseSettings):
 
     # 时间配置
     DATETIME_TIMEZONE: str = 'UTC'  # 如：Asia/Shanghai、UTC
-    DATETIME_FORMAT: str = "%Y-%m-%dT%H:%M:%S.%fZ"
+    DATETIME_FORMAT: str = "%Y-%m-%dT%H:%M:%S.%f%z"
 
     # 文件上传
     UPLOAD_READ_SIZE: int = 1024
