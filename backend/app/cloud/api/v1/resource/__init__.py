ca6f07cc078e17d84c3f4212dec1b881555d09b0
guhua <819070918@qq.com>
2026-07-24 09:31:35 +0800

    路径调整

--- backend/app/cloud/api/v1/resource/__init__.py
+++ backend/app/cloud/api/v1/resource/__init__.py
@@ -10,7 +10,6 @@ from fastapi import APIRouter
 
 from backend.app.cloud.api.v1.resource.audio import router as audio_router
 from backend.app.cloud.api.v1.resource.huoshan import router as huoshan_router
-from backend.app.cloud.api.v1.resource.toy import router as toy_router
 from backend.app.cloud.api.v1.resource.report import router as report_router
 from backend.app.cloud.api.v1.resource.script import router as script_router
 from backend.app.cloud.api.v1.resource.xiaozhi import router as xiaozhi_router
@@ -23,7 +22,6 @@ router.include_router(ximalaya_router, prefix='/ximalaya', tags=['喜马拉雅']
 router.include_router(xiaozhi_router, prefix='/xiaozhi', tags=['小智服务'])
 
 router.include_router(audio_router, prefix='/audio', tags=['音频'])
-router.include_router(toy_router, prefix='/toys', tags=['玩偶'])
 router.include_router(script_router, prefix='/scripts', tags=['剧本'])
 
 router.include_router(report_router, prefix='/report', tags=['使用报告'])
--- backend/app/cloud/api/v1/terminal/__init__.py
+++ backend/app/cloud/api/v1/terminal/__init__.py
@@ -13,6 +13,7 @@ from backend.app.cloud.api.v1.terminal.auth import router as auth_router
 from backend.app.cloud.api.v1.terminal.baby import router as baby_router
 from backend.app.cloud.api.v1.terminal.device_chat import router as device_chat_router
 from backend.app.cloud.api.v1.terminal.device import router as device_router
+from backend.app.cloud.api.v1.terminal.toy import router as toy_router
 from backend.app.cloud.api.v1.terminal.feedback import router as feedback_router
 from backend.app.cloud.api.v1.terminal.firmware import router as firmware_router
 from backend.app.cloud.api.v1.terminal.tool import router as tool_router
@@ -27,6 +28,7 @@ router.include_router(baby_router, prefix='/baby', tags=['宝宝管理'])
 router.include_router(device_chat_router, prefix='/device-chat', tags=['设备聊天'])
 router.include_router(feedback_router, prefix='/feedback', tags=['反馈管理'])
 router.include_router(device_router, prefix='/device', tags=['设备管理'])
+router.include_router(toy_router, prefix='/toys', tags=['玩偶'])
 router.include_router(firmware_router, prefix='/firmware', tags=['固件管理'])
 router.include_router(tool_router, prefix='/tool', tags=['三方工具'])
 router.include_router(led_router, prefix='/led', tags=['灯效'])
