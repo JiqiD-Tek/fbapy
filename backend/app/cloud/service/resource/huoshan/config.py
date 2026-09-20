1d30d9123a332c88407991bb43d7aa73d45df370
guhua <819070918@qq.com>
2026-09-20 15:11:27 +0800

    更新音色配置

--- backend/app/cloud/service/resource/huoshan/config.py
+++ backend/app/cloud/service/resource/huoshan/config.py
@@ -124,7 +124,7 @@ VOICE_PROJECTS: tuple[VoiceProject, ...] = (
         app_id=settings.JS61_BYTES_TTS_APPID,
         access_token=settings.JS61_BYTES_TTS_TOKEN,
         voices=_build_voice_profiles([
-            {'id': 'S_RCrqyDOZ1', 'name': '方厚鑫'},
+            {'id': 'S_RCrqyDOZ1', 'name': '旁大白'},
             {'id': 'S_fz5jyDOZ1', 'name': '英文男成'},
             {'id': 'S_jz2iyDOZ1', 'name': '英文儿童'},
             {'id': 'S_bpthyDOZ1', 'name': '虾球朋友旁白'},
